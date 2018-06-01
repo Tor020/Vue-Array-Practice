5a1dd7193c9260650e0547b749fb951f1e414ccf
Kevin <kopp020@gmail.com>
2018-06-01 18:55:39 -0400

    Update Home.vue

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -13,5 +13,59 @@ export default {
   components: {
 
   }
+  
+ //   el: '#root' /*the element to bind this to*/,
+    data: { 
+      // When a Vue instance is created, it adds all the properties found in its data object to Vue’s reactivity system
+      showModal: false,
+      placeHolderString: '',
+      placeHolderInteger: 0,
+      placeHolderBoolean: false,
+      placeHolderArray: [],
+      placeHolderObj: {},
+      placeHolderErrorRes: null
+    },
+    //Methods are called with functionality added onto the html
+    methods: {},
+    //Computed methods are called automatically.
+    computed: {},
+
+    //  //Instance Lifecycle Hooks
+
+    beforeCreate() {
+      /*  Init and Injections & Reactivity  */
+    },
+
+    created() {
+      /*  Init and Injections & Reactivity  */
+      console.log('created function check is: ' + this.placeHolderInteger);
+    },
+
+    beforeMount() {
+      /* before create root.$el and replace "root.el" with it */
+    },
+
+    mounted() {
+      /*  create root.$el and replace "root.el" with it   */
+    },
+
+    beforeUpdate() {
+      /*  When data changes but the virtual Dom hasn't re-rendered yet  */
+    },
+
+    updated() {
+      /*   When data changes and the virtual Dom has re-rendered  */
+    },
+
+    beforeDestroy() {
+      /*  Before Teardown watchers, child components and event listeners   */
+    },
+
+    destroyed() {
+      /*  After Teardown watchers, child components and event listeners  */
+    }
+
 }
+
+
 </script>
